8ec79f102f8fb022f1e5a3770efcb68e922a64ed
josejuarez <79205538+josejmill5@users.noreply.github.com>
2021-03-04 17:19:45 -0500

    Testing Github actions

---
+++ ConsoleCalculator/MyTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleCalculator
+{
+    public class MyTest
+    {
+        public string TestMethod()
+        {
+            string result = "no";
+            result = Environment.GetEnvironmentVariable("ImportantNumber");
+            return result;
+        }
+    }
+}
